ef2685b78604d31d111917b4d09c9a0f4b46794c
Kacper Stasica <kacperstasica@yahoo.pl>
2022-01-25 18:46:04 +0100

    Configure ALLOWED_HOSTS setting

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'ipstack-api-project.herokuapp.com']
+env.list('DJANGO_ALLOWED_HOSTS', default=[])
 
 # Application definition
 
